b13eedd6f75a6257c444ff9d1f72a4f81774e94b
Naman-Goel2911 <73833786+Naman-Goel2911@users.noreply.github.com>
2021-11-30 16:45:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request
+from classifier import getPred
+
+app = Flask(__name__)
+
+@app.route('/pred-data', methods = ['POST'])
+
+def pred_data():
+    image = request.files.get('digit')
+    prediction = getPred(image)
+    return jsonify({
+        'prediction':prediction
+    }),200
+
+if (__name__ == '__main__'):
+    app.run(debug=True)
---
+++ classifier.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import fetch_openml
+from PIL import Image
+import PIL.ImageOps
+
+x = np.load('image.npz')
+y = pd.read_csv('labels.csv')
+
+xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size = 2500, train_size= 7500, random_state = 9)
+xTrainScaled = xTrain/255
+xTestScaled = xTest/255
+
+lr = LogisticRegression(solver='saga', multi_class = 'multinomial').fit(xTrainScaled, yTrain)
+
+lr.fit(xTrainScaled, yTrain)
+
+def getPred(image):
+    im_pil = Image.open(image)
+    image_bw = im_pil.convert('L')
+    image_bw_resized = image_bw.resize((28, 28), Image.ANTIALIAS)
+    pixel_filter = 20
+    min_pixel = np.percentile(image_bw_resized, pixel_filter)
+    image_bw_resized_inverted_scaled = np.clip(image_bw_resized-min_pixel, 0, 255)
+    max_pixel = np.max(image_bw_resized)
+    image_bw_resized_inverted_scaled = np.asarray(image_bw_resized_inverted_scaled)/max_pixel
+
+    test_sample = np.array(image_bw_resized_inverted_scaled).reshape(1, 784)
+    test_pred = lr.predict(test_sample)
+    return test_pred[0]
